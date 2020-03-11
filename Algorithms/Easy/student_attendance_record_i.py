e9980347e663568115ee8f52841bb7613eb68ca1
Wattyyy <yuji.msec.ttasb@gmail.com>
2020-03-12 04:11:34 +0900

    added some files

---
+++ Algorithms/Easy/student_attendance_record_i.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/student-attendance-record-i/
+
+from collections import Counter
+class Solution:
+    def checkRecord(self, s):
+        cnt = Counter(s)
+        return (cnt['A'] <= 1) and ('LLL' not in s)
+        
