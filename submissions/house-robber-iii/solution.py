1b2b8b87b7a3cf5e9a307122aaebffc5992d11a0
Wattyyy <yuji.msec.ttasb@gmail.com>
2021-12-05 16:35:29 +0900

    add daily challenge subs

---
+++ submissions/house-robber-iii/solution.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    @lru_cache
+    def dp(self, node: Optional[TreeNode]) -> int:
+        if not node:
+            return 0
+
+        if not node.left and not node.right:
+            return node.val
+
+        if not node.left:
+            return max(
+                node.val + self.dp(node.right.left) + self.dp(node.right.right),
+                self.dp(node.right),
+            )
+
+        if not node.right:
+            return max(
+                node.val + self.dp(node.left.left) + self.dp(node.left.right),
+                self.dp(node.left),
+            )
+
+        return max(
+            node.val
+            + self.dp(node.left.left)
+            + self.dp(node.left.right)
+            + self.dp(node.right.left)
+            + self.dp(node.right.right),
+            self.dp(node.left) + self.dp(node.right),
+        )
+
+    def rob(self, root: Optional[TreeNode]) -> int:
+        return self.dp(root)
+
+
